f3da74d1955b5b61370dbe0ce3e53c98e68bf65d
Paul Zavadski <pavel.zavadski@cogniteq.com>
2023-05-13 16:57:24 +0300

    Add table data

---
+++ nested_set/nested_categories.sql
@@ -0,0 +1,10 @@
+INSERT INTO public.nested_categories (category_id, name, lft, rgt) VALUES (1, 'ELECTRONICS', 1, 20);
+INSERT INTO public.nested_categories (category_id, name, lft, rgt) VALUES (2, 'TVs', 2, 9);
+INSERT INTO public.nested_categories (category_id, name, lft, rgt) VALUES (3, 'TUBE', 3, 4);
+INSERT INTO public.nested_categories (category_id, name, lft, rgt) VALUES (4, 'LCD', 5, 6);
+INSERT INTO public.nested_categories (category_id, name, lft, rgt) VALUES (5, 'PLASMA', 7, 8);
+INSERT INTO public.nested_categories (category_id, name, lft, rgt) VALUES (6, 'PORTABLE', 10, 19);
+INSERT INTO public.nested_categories (category_id, name, lft, rgt) VALUES (7, 'MP3', 11, 14);
+INSERT INTO public.nested_categories (category_id, name, lft, rgt) VALUES (8, 'FLASH', 12, 13);
+INSERT INTO public.nested_categories (category_id, name, lft, rgt) VALUES (9, 'CD PLAYERS', 15, 16);
+INSERT INTO public.nested_categories (category_id, name, lft, rgt) VALUES (10, '2 WAY RADIOS', 17, 18);
